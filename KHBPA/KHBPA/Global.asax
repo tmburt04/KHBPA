cb667bed1dbecbfabec02412947bca63c137db8c
tmburt04 <tmburt04@cardmail.louisville.edu>
2017-09-06 16:34:44 -0400

    Initial Commit

---
+++ KHBPA/KHBPA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KHBPA.MvcApplication" Language="C#" %>
---
+++ KHBPA/KHBPA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KHBPA.Startup))]
+namespace KHBPA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
