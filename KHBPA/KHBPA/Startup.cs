91c34f852562a14745e52d4569a121b022e6cfeb
spencergrahamduncan <spencergrahamduncan@gmail.com>
2017-09-25 17:04:08 -0400

    Added Tyler's Roles to our solution

--- KHBPA/KHBPA/Startup.cs
+++ KHBPA/KHBPA/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using KHBPA.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(KHBPA.Startup))]
@@ -9,6 +12,58 @@ namespace KHBPA
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        public void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //If Admin role doesn't exist, create first Admin Role and a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+                //First we create Admin role   
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Then we create a Admin user                
+                var user = new ApplicationUser();
+                user.UserName = "admin@email.com"; //Use same UserName and Email for simplicity. 
+                user.Email = "admin@email.com";    //Else you will need to modify the login action in the AccountController
+                string userPWD = "Welcome1";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            //For adding more roles on startup
+            //// creating Creating Manager role    
+            //if (!roleManager.RoleExists("Manager"))
+            //{
+            //    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            //    role.Name = "Manager";
+            //    roleManager.Create(role);
+
+            //}
+
+            //// creating Creating Employee role    
+            //if (!roleManager.RoleExists("Employee"))
+            //{
+            //    var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            //    role.Name = "Employee";
+            //    roleManager.Create(role);
+
+            //}
+
         }
     }
 }
