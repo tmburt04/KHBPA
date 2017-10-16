204c60bdfcb7ac2989068cd8d5faa87a2b9df2cc
spencergrahamduncan <spencergrahamduncan@gmail.com>
2017-10-16 12:58:00 -0400

    Fixed Admin

---
+++ KHBPA/KHBPA/Migrations/201710161651399_removedNames.cs
@@ -0,0 +1,20 @@
+namespace KHBPA.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removedNames : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.AspNetUsers", "FirstName");
+            DropColumn("dbo.AspNetUsers", "LastName");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+        }
+    }
+}
