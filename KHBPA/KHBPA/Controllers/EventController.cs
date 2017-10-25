7deba602db25df2ffd9d34e3fbcf035e672c33bd
Vince Patrick <vince.patrick225@gmail.com>
2017-10-24 21:36:45 -0400

    Made Calendar admin friendly

--- KHBPA/KHBPA/Controllers/EventController.cs
+++ KHBPA/KHBPA/Controllers/EventController.cs
@@ -9,6 +9,7 @@ using DHTMLX.Scheduler;
 using DHTMLX.Scheduler.Data;
 using DHTMLX.Scheduler.Controls;
 
+
 namespace KHBPA.Controllers
 {
     public class EventController : Controller
@@ -17,22 +18,26 @@ namespace KHBPA.Controllers
         {
             var sched = new DHXScheduler(this);
             sched.Skin = DHXScheduler.Skins.Terrace;
-            sched.Config.hour_date = "%h:%i %A";
+            sched.Config.hour_date = "%h:%i %A";  // sets time to be AM,PM
             sched.LoadData = true;
             sched.EnableDataprocessor = true;
-            sched.Highlighter.Enable("highlight_section");
+            sched.Highlighter.Enable("highlight_section");  // can create event from highlighted area
             sched.Lightbox.Add(new LightboxText("location", "Location") { Height = 50 });
-            var map = new MapView { ApiKey = "" };
+            var map = new MapView { ApiKey = "AIzaSyAiVPuK6Dil2M2QOdY38iqjvmdFwn3RTDQ" };
             sched.Views.Add(map);
             sched.Lightbox.AddDefaults();
             sched.Config.first_hour = 5;
+
             if (!User.IsInRole("Admin"))
             {
-                sched.Config.readonly_form = true;
+                sched.Config.readonly_form = true;  // users cannot add, delete, edit events through lightbox               
+                sched.Config.icons_select = new EventButtonList{
+                        EventButtonList.Details                 // users can only get details, not delete or edit
+                        };
             }
             else
             {
-                sched.Lightbox.Add(new LightboxText("lat", "Lat") { Height = 50 });
+                sched.Lightbox.Add(new LightboxText("lat", "Lat") { Height = 50 });  // only admin can see these fields
                 sched.Lightbox.Add(new LightboxText("lng", "Long") { Height = 50 });
             }
             sched.InitialView = "month";
@@ -53,30 +58,33 @@ namespace KHBPA.Controllers
             var action = new DataAction(actionValues);
             var changedEvent = DHXEventsHelper.Bind<Event>(actionValues);
             var entities = new ApplicationDbContext();
-            try
+            if (User.IsInRole("Admin"))
             {
-                switch (action.Type)
+                try
                 {
-                    case DataActionTypes.Insert:
-                        entities.Event.Add(changedEvent);
-                        break;
-                    case DataActionTypes.Delete:
-                        changedEvent = entities.Event.FirstOrDefault(ev => ev.id == action.SourceId);
-                        entities.Event.Remove(changedEvent);
-                        break;
-                    default:// "update"
-                        var target = entities.Event.Single(e => e.id == changedEvent.id);
-                        DHXEventsHelper.Update(target, changedEvent, new List<string> { "id" });
-                        break;
+                    switch (action.Type)
+                    {
+                        case DataActionTypes.Insert:
+                            entities.Event.Add(changedEvent);
+                            break;
+                        case DataActionTypes.Delete:
+                            changedEvent = entities.Event.FirstOrDefault(ev => ev.id == action.SourceId);
+                            entities.Event.Remove(changedEvent);
+                            break;
+                        default:// "update"
+                            var target = entities.Event.Single(e => e.id == changedEvent.id);
+                            DHXEventsHelper.Update(target, changedEvent, new List<string> { "id" });
+                            break;
+                    }
+                    entities.SaveChanges();
+                    action.TargetId = changedEvent.id;
                 }
-                entities.SaveChanges();
-                action.TargetId = changedEvent.id;
-            }
-            catch (Exception a)
-            {
-                action.Type = DataActionTypes.Error;
-            }
 
+                catch (Exception a)
+                {
+                    action.Type = DataActionTypes.Error;
+                }
+            }
             return (new AjaxSaveResponse(action));
         }
     }
